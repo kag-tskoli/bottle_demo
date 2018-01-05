948e684f35d8cc12e2bc4e93bc8d3c04d6f896c4
Karl Ágústsson <kag@tskoli.is>
2018-01-05 14:41:15 +0000

    Update app.py

--- app.py
+++ app.py
@@ -6,8 +6,6 @@ from bottle import run, route
 def index():
   return "Hello bottle <br> New Line"
 
-if os.environ.get('APP_LOCATION') == 'heroku':
-    run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
-else:
-    run(host='localhost', port=8080, debug=True)
+run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+
 
