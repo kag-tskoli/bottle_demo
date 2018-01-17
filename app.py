ca4946f8b2781c859c78bc430a71a95c274169a2
Karl Ágústsson <kag@tskoli.is>
2018-01-17 08:05:49 +0000

    Update app.py

--- app.py
+++ app.py
@@ -2,9 +2,12 @@ import os
 from bottle import run, route
 
 @route('/')
-
 def index():
-  return "Hello bottle <br> New Line"
+  return "<a href='/about'>About</a>"
+
+@route('/about')
+def about():
+  return "<h2>About</h2>"
 
 run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
 
